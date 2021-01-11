f5f35cc7ba474293def4cd3d2311361d8c1a5cfb
momoka-mero <momokakj8.love@gmail.com>
2021-01-11 07:56:03 +0000

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
 
 
   has_many :logs, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_logs, through: :likes, source: :log
-  belongs_to :target, optional: true
+  #belongs_to :target, optional: true
 
 
 
