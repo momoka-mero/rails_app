dea427f4f229cdb4cafe3a0501ead45ba5183c25
momoka-mero <momokakj8.love@gmail.com>
2021-01-11 09:54:40 +0000

    complete?

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,10 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Target.create(weight:"痩せたい")
+Target.create(weight:"現状維持")
+Target.create(weight:"増量したい")
